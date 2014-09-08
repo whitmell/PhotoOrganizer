e883d4725560873fb3228a9f8d9c8dccc640ec00
whitmell <whitmell@gmail.com>
2014-09-08 08:42:45 -0400

    Initial checkin

---
+++ PhotoOrganizer/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PhotoOrganizer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.btnBrowseSource = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.btnBrowseDest = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Message = new System.Windows.Forms.Label();
+            this.copyCheck = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(117, 25);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(295, 20);
+            this.txtSource.TabIndex = 0;
+            // 
+            // btnBrowseSource
+            // 
+            this.btnBrowseSource.Location = new System.Drawing.Point(427, 25);
+            this.btnBrowseSource.Name = "btnBrowseSource";
+            this.btnBrowseSource.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseSource.TabIndex = 1;
+            this.btnBrowseSource.Text = "Browse...";
+            this.btnBrowseSource.UseVisualStyleBackColor = true;
+            this.btnBrowseSource.Click += new System.EventHandler(this.btnBrowseSource_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Select a source";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(221, 158);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "GO!";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select a destination";
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(118, 80);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(295, 20);
+            this.txtDest.TabIndex = 5;
+            // 
+            // btnBrowseDest
+            // 
+            this.btnBrowseDest.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btnBrowseDest.Location = new System.Drawing.Point(427, 80);
+            this.btnBrowseDest.Name = "btnBrowseDest";
+            this.btnBrowseDest.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseDest.TabIndex = 6;
+            this.btnBrowseDest.Text = "Browse...";
+            this.btnBrowseDest.UseVisualStyleBackColor = true;
+            this.btnBrowseDest.Click += new System.EventHandler(this.btnBrowseDest_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // Message
+            // 
+            this.Message.AutoSize = true;
+            this.Message.Location = new System.Drawing.Point(16, 129);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(19, 13);
+            this.Message.TabIndex = 8;
+            this.Message.Text = "lml";
+            // 
+            // copyCheck
+            // 
+            this.copyCheck.AutoSize = true;
+            this.copyCheck.Checked = true;
+            this.copyCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.copyCheck.Location = new System.Drawing.Point(19, 158);
+            this.copyCheck.Name = "copyCheck";
+            this.copyCheck.Size = new System.Drawing.Size(86, 17);
+            this.copyCheck.TabIndex = 9;
+            this.copyCheck.Text = "Make Copy?";
+            this.copyCheck.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 193);
+            this.Controls.Add(this.copyCheck);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBrowseDest);
+            this.Controls.Add(this.txtDest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowseSource);
+            this.Controls.Add(this.txtSource);
+            this.Name = "frmMain";
+            this.Text = "Photo Organizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnBrowseSource;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Button btnBrowseDest;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.CheckBox copyCheck;
+    }
+}
+
---
+++ PhotoOrganizer/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+using com.drew.lang;
+using com.drew.metadata;
+using com.drew.metadata.exif;
+using com.drew.imaging.jpg;
+using com.drew.imaging.tiff;
+
+using com.utils;
+using com.utils.bundle;
+using com.utils.xml;
+using System.Globalization;
+
+namespace PhotoOrganizer
+{
+    public partial class frmMain : Form
+    {
+        private const string _logFileAddr = @"Logfile.txt";
+        private string _sourceFolder, _destFolder;
+        private StringBuilder log;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            Message.Text = string.Empty;
+            txtSource.Text = @"C:\Users\Whit\Pictures\";
+            txtDest.Text = @"C:\Users\Whit\Pictures\";
+            log = new StringBuilder();
+        }
+
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtSource.Text) || string.IsNullOrEmpty(txtDest.Text))
+            {
+                Message.Text = "Please enter valid source and destination folders.";
+                return;
+            }
+
+            _sourceFolder = txtSource.Text;
+            _destFolder = txtDest.Text;
+            txtSource.Enabled = false;
+            txtDest.Enabled = false;
+            Message.Text = "Organizing photos, please wait...";
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+            worker.DoWork += new DoWorkEventHandler(DoWork); //(txtSource.Text, txtDest.Text));
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Completed);
+
+            try
+            {
+                worker.RunWorkerAsync();
+            }
+            catch(Exception ex)
+            {
+                Message.Text = "ERROR: " + ex.Message;
+                txtSource.Enabled = true;
+                txtDest.Enabled = true;
+            }
+        }
+
+        private void DoWork(object sender, DoWorkEventArgs args)
+        {
+            BackgroundWorker worker = (BackgroundWorker)sender;
+
+            OrganizeFolder(_sourceFolder, _destFolder);
+            using (StreamWriter w = File.AppendText(_destFolder + "\\" + _logFileAddr))
+            {
+                w.WriteLine(log.ToString());
+                w.Flush();
+                w.Close();
+            }
+            
+        }
+        private void Completed(object sender, RunWorkerCompletedEventArgs args)
+        {
+            if (args.Error != null && !string.IsNullOrEmpty(args.Error.Message))
+            {
+                Message.Text = "ERROR: " + args.Error.Message;
+            }
+            else
+            {
+                Message.Text = "Completed Successfully!";
+            }
+
+            txtDest.Enabled = true;
+            txtSource.Enabled = true;
+        }
+
+        private void OrganizeFolder(string sourceFolder, string destFolder)
+        {
+            List<string> files = Directory.EnumerateFiles(sourceFolder, "*.*", SearchOption.AllDirectories).ToList();
+            List<string> subdirs = Directory.GetDirectories(sourceFolder).ToList();
+            List<string> ignored = new List<string>();
+            //Image img;
+            //PropertyItem prop = null;
+
+            FileInfo fi = null;
+            Metadata data = null;
+            bool isImage = false;
+            DateTime date = DateTime.MinValue;
+            string dateTaken = string.Empty, dateDigitized = string.Empty,
+                dateModified = string.Empty, dateCreated = string.Empty;
+
+
+            foreach (string fileName in files)
+            {
+                try
+                {
+                    fi = new FileInfo(fileName);
+
+                    if (IsFileType(fileName, "JPEG"))
+                    {
+                        /*
+                        img = Image.FromFile(fileName);
+                        try
+                        {
+                            if (img.PropertyIdList.Contains(int.Parse("9003", System.Globalization.NumberStyles.AllowHexSpecifier)))
+                            {
+                                prop = img.GetPropertyItem(int.Parse("9003", System.Globalization.NumberStyles.AllowHexSpecifier));
+                            }
+                            else if (img.PropertyIdList.Contains(int.Parse("9004", System.Globalization.NumberStyles.AllowHexSpecifier)))
+                            {
+                                prop = img.GetPropertyItem(int.Parse("9004", System.Globalization.NumberStyles.AllowHexSpecifier));
+                            }
+                            else if (img.PropertyIdList.Contains(int.Parse("0132", System.Globalization.NumberStyles.AllowHexSpecifier)))
+                            {
+                                prop = img.GetPropertyItem(int.Parse("0132", System.Globalization.NumberStyles.AllowHexSpecifier));
+                            }
+                            else if (File.GetLastWriteTime(fileName) != DateTime.MinValue)
+                            {
+                                strDate = File.GetLastWriteTime(fileName).ToString("yyyyMMdd");
+                            }
+
+                            img.Dispose();
+
+                            if (prop != null)
+                            {
+                                byte[] asciiBytes = prop.Value;
+                                strDate = System.Text.Encoding.ASCII.GetString(asciiBytes);
+                                strDate = strDate.Substring(0, strDate.IndexOf(" ")).Replace(":", "");
+                            }
+
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                        finally
+                        {
+                            if (img != null)
+                            {
+                                img.Dispose();
+                            }
+                        }
+                        */
+                        isImage = true;
+                        data = JpegMetadataReader.ReadMetadata(fi);
+
+                    }
+                    else if (IsFileType(fileName, "RAW"))
+                    {
+                        //handle Raws
+                        isImage = true;
+                        data = TiffMetadataReader.ReadMetadata(fi);
+
+                    }
+
+                    if (data != null)
+                    {
+                        // Get date taken
+                        foreach (AbstractDirectory lcDirectory in data)
+                        {
+                            string strDate;
+                            // We look for potential error
+                            if (lcDirectory.HasError)
+                            {
+                                Debug.WriteLine("Some errors were found, activate trace using /d:TRACE option with the compiler");
+                            }
+
+                            foreach (Tag t in lcDirectory.Where(x => x.GetTagName().ToUpper().Contains("DATE")))
+                            {
+                                strDate = t.GetDescription();
+                                strDate = strDate.Substring(0, strDate.IndexOf(" ")).Replace(":", "\\");
+
+                                if (t.GetTagName().ToUpper().Contains("ORIGINAL"))
+                                {
+                                    dateTaken = strDate;
+                                }
+                                if (t.GetTagName().ToUpper().Contains("DIGITIZED"))
+                                {
+                                    dateDigitized = strDate;
+                                }
+                            }
+                        }
+                        data = null;
+                    }
+
+                    if (isImage)
+                    {
+                        DateTime.TryParse(dateTaken, out date);
+                        if (DateTime.MinValue == date)
+                            DateTime.TryParseExact(dateTaken.Replace("\\", string.Empty), "yyyyMMdd", CultureInfo.InvariantCulture,
+                           DateTimeStyles.None, out date);
+                        if (DateTime.MinValue == date)
+                            DateTime.TryParse(dateDigitized, out date);
+                        if (DateTime.MinValue == date)
+                            DateTime.TryParseExact(dateDigitized.Replace("\\", string.Empty), "yyyyMMdd", CultureInfo.InvariantCulture,
+                           DateTimeStyles.None, out date);
+                        if (DateTime.MinValue == date)
+                            date = (fi.CreationTime < fi.LastWriteTime) ? fi.CreationTime : fi.LastWriteTime;
+
+                        string s = CopyPicTo(fileName, date, destFolder);
+                        if (!string.IsNullOrEmpty(s))
+                            log.AppendLine(s);
+                    }
+                    else
+                    {
+                        ignored.Add(fileName);
+                    }
+
+                    isImage = false;
+
+                }
+                catch(Exception ex)
+                {
+                    throw ex;
+                }
+                
+            }
+
+            foreach(string dir in subdirs)
+            {
+                OrganizeFolder(dir, destFolder);
+            }
+
+            if (ignored.Count > 0)
+            {
+                log.AppendLine("Ignored files in :" + sourceFolder);
+                foreach (string s in ignored)
+                {
+                    log.AppendLine("   - " + s);
+                }
+            }
+        }
+
+        private bool IsFileType(string fileName, string type)
+        {
+            if (type == "JPEG")
+            {
+                return fileName.ToUpper().EndsWith("JPG") ||
+                    fileName.ToUpper().EndsWith("JPEG");
+            }
+            if (type == "RAW")
+            {
+                return fileName.ToUpper().EndsWith("CR2") ||
+                    fileName.ToUpper().EndsWith("CRW") ||
+                    fileName.ToUpper().EndsWith("RAW");
+            }
+            if (type == "VID")
+            {
+                return fileName.ToUpper().EndsWith("AVI") ||
+                    fileName.ToUpper().EndsWith("MOV") ||
+                    fileName.ToUpper().EndsWith("OGG") ||
+                    fileName.ToUpper().EndsWith("MPG") ||
+                    fileName.ToUpper().EndsWith("MPEG") ||
+                    fileName.ToUpper().EndsWith("M4V");
+            }
+            return false;
+        }
+
+        private string CopyPicTo(string fileName, DateTime date, string destFolder)
+        {
+            string imgName = fileName.Substring(fileName.LastIndexOf(@"\") + 1);
+            StringBuilder errors = new StringBuilder();
+            string sourceFolder;
+            if (DateTime.MinValue == date)
+            {
+                sourceFolder = "Unknown";
+            }
+            else
+            {
+                sourceFolder = date.Year.ToString() + @"\" + date.ToString("MM") + @"\" + date.ToString("yyyyMMdd");
+            }
+            sourceFolder = destFolder + @"\" + sourceFolder;
+            bool failure = false;
+
+            if (!Directory.Exists(sourceFolder))
+            {
+                try
+                {
+                    Directory.CreateDirectory(sourceFolder);
+                }
+                catch (Exception ex)
+                {
+                    failure = true;
+                    errors.AppendLine("Could not create directory " + sourceFolder + " - " + ex.Message);
+                }
+            }
+
+            if (!failure)
+            {
+                if(File.Exists(sourceFolder + @"\" + imgName))
+                {
+                    errors.AppendLine("File already exists:" + sourceFolder + @"\" + imgName);                  
+                }
+                else
+                {
+                    try
+                    {
+                        if (copyCheck.Checked)
+                            File.Copy(fileName, sourceFolder + @"\" + imgName);
+                        else
+                            File.Move(fileName, sourceFolder + @"\" + imgName);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception("Error copying file " + imgName + " - " + ex.Message);
+                    }
+                }
+            }
+
+            return errors.ToString();
+        }
+
+        private void btnBrowseSource_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                txtSource.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnBrowseDest_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog2.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                txtDest.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+    }
+}
